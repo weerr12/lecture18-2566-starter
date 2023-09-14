d4e0916880fa223f316dd433eab9f5a17f22f1f4
weerr12 <jokerwwe0542@gmail.com>
2023-09-14 13:07:17 +0700

    lecture18

--- src/app/api/enrollment/route.js
+++ src/app/api/enrollment/route.js
@@ -1,14 +1,43 @@
 import { DB } from "@/app/libs/DB";
 import { NextResponse } from "next/server";
-
+import { headers } from "next/headers";
+import jwt from "jsonwebtoken";
+// api debug ส่งข้อมูลทั้งหมดใน DB ออกมา
 export const GET = async (request) => {
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
+  // ดูวิชาของเจ้าของ token -> step 1 อ่าน token มาให้ได้ก่อน
+  // import headers ด้วย
+  const rawAuthHeader = headers().get("authorization");
+  // ลบคำว่า Bearer ทิ้งไป ที่เราไปแปะ
+  const token = rawAuthHeader.split(" ")[1];
+  // การแนบ token ก็อป token จาก login ไปวางใน GET enroll แปะใน Auth -> Bearer Token วางตรง Token เลย
+  // ย่อ ไปสร้าง ตัวแปร token เพิ่มใน ฟันเฟือง แล้วเอา token ไปแปะ -> เวลาเรียกใช้ {{token}}
+  let studentId = null;
+
+  // ดูว่า token จริงหรือเปล่า
+  // ทดสอบว่า token เนี่ยเป็น token ที่ถูกสร้างด้วย secret อันเนี่ยเปล่า ->token, process.env.JWT_SECRET
+  // try catch -> ดัก runtime error ถ้าตรง try error กระโดดไปที่ catch เลย
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // ถ้าเอา payload ออกมาได้
+    // เราจะดึง studentId ของคนที่ login ออกมาดู
+    studentId = payload.studentId;
+    // ผลลัพธ์
+    // {
+    //   "ok": true,
+    //   "courseNoList": [
+    //     "261207",
+    //     "001101"
+    //   ]
+    // }
+  } catch {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
+  }
 
   const courseNoList = [];
   for (const enroll of DB.enrollments) {
@@ -23,6 +52,22 @@ export const GET = async (request) => {
 };
 
 export const POST = async (request) => {
+  const rawAuthHeader = headers().get("authorization");
+  const token = rawAuthHeader.split(" ")[1];
+  let studentId = null;
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    studentId = payload.studentId;
+  } catch {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
+  }
   //read body request
   const body = await request.json();
   const { courseNo } = body;
--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -1,17 +1,44 @@
+import { DB } from "@/app/libs/DB";
 import { NextResponse } from "next/server";
+// import token ด้วยตัวเองตรงนี้
+import jwt from "jsonwebtoken";
 
 //POST /api/user/login
 export const POST = async (request) => {
   const body = await request.json();
+  // แกะ username กับ password มา
   const { username, password } = body;
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Username or password is incorrect",
-  //   },
-  //   { status: 400 }
-  // );
+  // ควรจะเช็ค varidate แต่ข้ามไปทำบ่อยละ
 
-  return NextResponse.json({ ok: true });
+  // ค้นหาว่ามี user ที่มี userpasswprd ตามนี้หรือเปล่า
+  const user = DB.users.find(
+    (user) => user.username === username && user.password === password
+  );
+  // ดักกรณี หา user ไม่เจอ
+  if (!user)
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Username or password is incorrect",
+      },
+      { status: 400 }
+    );
+  // สมมุติว่า เช็คแล้วเจอ user จริงๆ -> สร้าง token ขึ้นมาเลย
+  const token = jwt.sign(
+    // มันจะรับ paramitter 3 ตัว
+    // 1.payload -> data ที่อยากจะฝังเข้าไปใน token
+    {
+      username,
+      role: user.role,
+      studentId: user.studentId,
+    },
+    // 2.secret -> file env
+    process.env.JWT_SECRET,
+    // 3.option ในการสร้าง token
+    // หมดอายุเมื่อไหร่
+    { expiresIn: "8h" }
+  );
+  // ส่ง token กลับไปให้ user
+  return NextResponse.json({ ok: true, token });
 };
